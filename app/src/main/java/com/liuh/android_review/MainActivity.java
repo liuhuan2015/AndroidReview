6cc7fccb26027368babadb59ef67e6fd69c18938
liuh <2210712252@qq.com>
2018-11-05 15:54:26 +0800

    数据结构和算法题

--- app/src/main/java/com/liuh/android_review/MainActivity.java
+++ app/src/main/java/com/liuh/android_review/MainActivity.java
@@ -2,6 +2,11 @@ package com.liuh.android_review;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> hashMap = new HashMap<>();
+
+        Map<String, String> hashTable = new Hashtable<>();
+
+        SparseArray<String> sparseArray = new SparseArray<>();
+
+
     }
 }
